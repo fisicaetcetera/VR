1180cebdfe7c05fa7bf319e77edffb41d7d6da9d
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2022-11-15 16:15:40 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 function preload() {
+  
+  sun = loadImage('sun.jpg');
   createVRCanvas();
 }
 
 function setup() {
   setVRBackgroundColor(200, 0, 150);
-  fill(0, 255, 0);
+  fill(0);
 }
 
 function calculate() {
@@ -15,6 +17,6 @@ function draw() {
   translate(0, 0, 10);
   rotateX(10);
   rotateY(20);
-  
-  box(5);
+  texture(sun);
+  sphere(15);
 }
