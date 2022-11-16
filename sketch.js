ba8e1db29b416cd09b2f2e479a79bc0a7b8c0c8d
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2022-11-15 21:01:52 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,16 +6,16 @@ function preload() {
 
 function setup() {
   setVRBackgroundColor(0);
+  let ii = 1;
 }
 
 function calculate() {
   // Things you want to happen once per frame
-  rotateY(0.5);
 }
 
 function draw() {
-  translate(0, 0, 130);
-  //rotateY(frameCount/100);
+  translate(0, 0, -50);
+  rotateY(ii++/100);
   texture(sun);
   sphere(10);
 }
