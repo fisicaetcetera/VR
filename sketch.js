faa31a1eb91adc33aff80a3a23eae2bca6322cf7
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2023-08-02 11:54:44 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,22 @@
+let moon;
+
 function preload() {
-  
-  sun = loadImage('sun.jpg');
-    createVRCanvas();
-  setVRBackgroundColor("blue");
+  createVRCanvas();
+  moon = loadImage("moonmap1k.jpg");
 }
 
 function setup() {
-  //background(0);
-  let ii = 1;
+  setVRBackgroundColor(0,0,0);
+
 }
+
 function draw() {
-  setViewerPosition(0, 0, 1);
-  //push();
-  translate(0, 0, -30);
-  rotateY(ii++/1000);
-  //texture(sun);
-  //noStroke();
-  stroke(0);
-  sphere(10);
-  //pop();
-}
+  setViewerPosition(0, 0, 200);
+  push();
+    translate(0,0,0);
+    texture(moon);
+    noStroke();
+    sphere(70);
+  pop();
+  
+} //draw
