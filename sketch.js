4385f85aa166f38431a36eb7197105951cbd81c2
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2023-09-04 11:10:51 -0300

    new sketch, simpler

--- sketch.js
+++ sketch.js
@@ -1,45 +1,28 @@
-let moon;
-let venus;
-let stars;
+let randomx=[], randomy=[];
 
 function preload() {
   createVRCanvas();
-  moon = loadImage("moonmap1k.jpg");
-  stars = loadImage("whole-sky-nasa.jpg");
-  sun = loadImage("sun.jpg");
 }
 
 function setup() {
-  setVRBackgroundColor(0,0,0);
-
+  setVRBackgroundColor(200, 0, 150);
+  for(let i=0; i<5; ++i) {
+    randomx[i] = random(-500, 500);
+    randomy[i] = random(-500, 500);
+  }
+  
 }
 
 function draw() {
-  setViewerPosition(0, 0, 0);
-  push();
-    translate(0,0,-100);
-    texture(moon);
-    noStroke();
-    rotateY(frameCount/1000);
-    //sphere(30);
-  pop();
-
-  
-  push();
- 
-    translate(0,-100,-100);
-    //rotateY(1.507);
-    texture(sun);
-    noStroke();
-    rotateY(frameCount/1000);
-    sphere(30);
-  pop();
-
-
-   push();
-  translate(0, 0, 0);
-      rotateX(0.3);
-  texture(stars);
-  sphere(1500);
-  pop();
-} //draw
+  setViewerPosition(0, 0, 400);
+  for(let i=0; i<5; ++i) {
+    push();
+    translate(randomx[i], randomy[i]);
+    fill('red');
+    if(intersectsSphere(70, 0, 0)) {
+      fill('blue');
+    }
+    sphere(70);
+    pop();
+  }
+}
