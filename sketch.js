56f6eb84e5deaefff07481d7a7d3f2b264390c72
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2023-09-04 10:38:39 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let venus;
 let stars;
 
 function preload() {
-  createVRCanvas(2000);
+  createVRCanvas(3000);
   moon = loadImage("moonmap1k.jpg");
   stars = loadImage("whole-sky-nasa.jpg");
   sun = loadImage("sun.jpg");
