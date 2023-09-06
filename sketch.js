3cc4494d5c681294bd0a6ae36496e297b726d627
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2023-09-05 21:16:31 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -42,11 +42,11 @@ function draw() {
     translate(x[i], y[i], z[i]/50);
     noStroke();
     if(intersectsSphere(70, 0, 0)) {
-       //textFont(myfont, 10 );
-       textFont('Georgia');
+       textFont(myfont, 10 );
+       //textFont('Georgia');
        text("Planet name...", x[i]+40, y[i]+40) ;
        strokeWeight(1);
-       stroke(255);
+       stroke(255,255,255,0.5);
     }
     ii = random(numeros);
     texture(planets[i]);
